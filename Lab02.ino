4123d33f914c57f7e6b9b4a01cbb830b7de1fca2
dennis-max <57693915+dennis-max@users.noreply.github.com>
2020-03-11 22:57:27 +0800

    Add files via upload

--- Lab02.ino
+++ Lab02.ino
@@ -1,61 +1,78 @@
 const byte LEDs[] =
-{2,3,4,5,6,7,8,9};
+{2,3,4,5,8,9,10,11};
+const byte Button=6;
 const byte total = sizeof(LEDs);
 int index = 0;
 int i=0;
 int j=0;
 void setup() {
-  for (byte i=0;i<total;i++) {
-pinMode(LEDs[i], OUTPUT);
-      };
-  for(i=0; i<total; i++)
-  {
-     digitalWrite(LEDs[i],LOW);
-  }
-}
-
-void loop() {
-  for (j=0; j<= 1; j++)
-  {
-    for ( index=0;index<=7;index++) 
+  pinMode(Button,INPUT);
+for (byte i=0;i<total;i++) {
+  pinMode(LEDs[i], OUTPUT); 
+};
+for (i=0;i<total;i++) 
     {
-digitalWrite(LEDs[index], HIGH);
-  }
-  delay(100);
-  for ( index=0;index<=7;index++) 
-  {
-    digitalWrite(LEDs[index], LOW);
+       digitalWrite(LEDs[i], LOW);
     }
-  delay(100);
-  }
-
-  
-  for ( index=0;index<=7;index++)
-  {
-    digitalWrite(LEDs[index], HIGH);
-    delay(100);
-    digitalWrite(LEDs[index], LOW);
-  }
-for(j=0;j<=1;j++)
+}
+void loop()
 {
-  for ( index=0;index<=7;index++)
-  {
-    digitalWrite(LEDs[index], HIGH);
-  }
-  delay(100);
-  for ( index=0;index<=7;index++)
-  {
-    digitalWrite(LEDs[index], LOW);
-  }
-  delay(100);
-  }
-
+  boolean val = digitalRead(6);
+  if(val){
+            for(j=0;j<=1;j++)
+            {
+                    for (index=0; index <= 7; index++) 
+                   { 
+                           digitalWrite(LEDs[index], HIGH);
+                   }
+                   delay(100);
+                   for (index=0; index <= 7; index++) 
+                   { 
+                           digitalWrite(LEDs[index], LOW);
+                   }
+                   delay(100);
+            }
+            for (index=0; index <= 7; index++) 
+            { 
+                    digitalWrite(LEDs[index], HIGH);
+                    delay(100);
+                    digitalWrite(LEDs[index], LOW);
+            }
+            
+            for(j=0;j<=1;j++)
+            {
+                    for (index=0; index <= 7; index++) 
+                   { 
+                            digitalWrite(LEDs[index], HIGH);
+                   }
+                   delay(100);
+                   for (index=0; index <= 7; index++) 
+                   { 
+                            digitalWrite(LEDs[index], LOW);
+                   }
+                   delay(100);
+            }
+          
+                    for (index=7; index >=0; index--) 
+                    { 
+                           digitalWrite(LEDs[index], HIGH);
+                           delay(100);
+                           digitalWrite(LEDs[index], LOW);
+                    }
+          }
+ else{
+  for (index=0; index <= 7; index++) 
+         { 
+          digitalWrite(LEDs[index], HIGH);
+          delay(100);
+          digitalWrite(LEDs[index], LOW);
+         }
   
-  for( index=7;index>=0;index--)
-  {
-    digitalWrite(LEDs[index], HIGH);
-    delay(100);
-    digitalWrite(LEDs[index], LOW);
-    
+          for (index=7; index >=0; index--) 
+          { 
+          digitalWrite(LEDs[index], HIGH);
+          delay(100);
+          digitalWrite(LEDs[index], LOW);
+          }
   }
 }
